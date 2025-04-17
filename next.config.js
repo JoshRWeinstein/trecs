18d5f5fc27d6aa8823210e5c146d8c47ba543f9b
joshrweinstein <joshrweinstein@gmail.com>
2025-04-16 21:04:19 -0400

    fix: remove static export settings and optimize for Vercel

--- next.config.js
+++ next.config.js
@@ -1,69 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.svgrepo.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'imgcdn.stablediffusionweb.com',
-      }
-    ],
-    unoptimized: true,
     domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        path: false,
-      }
-    }
-    return config
-  },
   experimental: {
-    serverComponentsExternalPackages: ['@prisma/client'],
     serverActions: true,
+    serverComponentsExternalPackages: ['@prisma/client'],
   },
   typescript: {
     ignoreBuildErrors: true,
   },
-  output: 'export',
-  basePath: '/trecs',
-  assetPrefix: '/trecs/',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },
-      '/my-recs': { page: '/my-recs' },
-      '/search': { page: '/search' },
-      '/[slug]': { page: '/[slug]' },
-    }
-  }
 }
 
 module.exports = nextConfig 
